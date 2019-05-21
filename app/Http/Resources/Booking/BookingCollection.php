0e2e46fb43793e419cd040fd4a61df17f9794ed3
Obinna Elvis Okechukwu <dreamor47@yahoo.com>
2019-05-21 17:08:01 +0100

    Encapsulate data names in resources (BookingCollection)

--- app/Http/Resources/Booking/BookingCollection.php
+++ app/Http/Resources/Booking/BookingCollection.php
@@ -17,8 +17,9 @@ class BookingCollection extends Resource
         return [
             'customer_name' =>$this->customer->first_name.' '.$this->customer->last_name,
             'date' => $this->time,
-            'cost' => $this->cost,
-            'duration' => $this->booking_time->duration,
+            'total_cost' => $this->cost,
+            'booking_type' => $this->booking_time->name,
+            'booking_hours' => $this->booking_time->duration,
             'href' => [
                 'customer_booking' => route('bookings.index', $this->customer->id)
             ]
